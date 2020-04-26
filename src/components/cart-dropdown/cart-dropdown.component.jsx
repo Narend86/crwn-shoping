d92c81a70cfca657715f67f955c6b25039471425
narendra <narendra@bitcot.com>
2020-04-26 20:31:24 +0530

    create cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component.jsx';
+import CartItem from '../cart-item/cart-item.component.jsx';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropDown =()=>(
-    <div className='cart-dropdown'>
-    <div className='cart-items' />
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
-);
 
-export default CartDropDown;
+const CartDropDown = ({ cartItems }) => {
+    console.log(cartItems)
+    return (
+        <div className='cart-dropdown'>
+            <div className='cart-items'>
+        {
+            cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))
+        }
+        </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>)
+};
+
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+export default connect(mapStateToProps)(CartDropDown);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,7 +6,7 @@ export const addItemToCart = (cartItems, cartItemsToAdd) =>{
         return cartItems.map(cartItem => 
             cartItem.id === cartItemsToAdd.id
             ? {...cartItem, quantity:cartItem.quantity+1}
-            :{cartItem}
+            : cartItem
             )
     }
     return [...cartItems, {...cartItemsToAdd, quantity:1}]
